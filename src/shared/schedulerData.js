17546d9c43c2fea60d379603af02d00af8bcebf5
Giulio Mario Martena <giuliomariomartena@gmail.com>
2020-10-01 14:04:49 +0200

    Hard coded palinsesto

--- src/shared/schedulerData.js
+++ src/shared/schedulerData.js
@@ -1,14 +1,159 @@
+const MONDAY = [
+  {
+    startDate: "2020-10-04T15:00:00.000Z",
+    endDate: "2020-10-04T16:00:00.000Z",
+    title: "Dischi Volandi",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=MO',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T16:00:00.000Z",
+    endDate: "2020-10-04T17:00:00.000Z",
+    title: "Cosplay Log",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=MO',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T17:00:00.000Z",
+    endDate: "2020-10-04T18:00:00.000Z",
+    title: "Mood Crush",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=MO',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T18:00:00.000Z",
+    endDate: "2020-10-04T19:00:00.000Z",
+    title: "C.I.C.",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=MO',
+    room: 1
+  }
+]
+
+const TUESDAY = [
+  {
+    startDate: "2020-10-04T12:00:00.000Z",
+    endDate: "2020-10-04T13:00:00.000Z",
+    title: "Pills",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=TU',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T17:00:00.000Z",
+    endDate: "2020-10-04T18:00:00.000Z",
+    title: "Sporting Club",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=TU',
+    room: 1
+  }
+]
+
+const WEDNESDAY = [
+  {
+    startDate: "2020-10-04T12:00:00.000Z",
+    endDate: "2020-10-04T13:00:00.000Z",
+    title: "NECST Tech Time",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=WE',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T16:00:00.000Z",
+    endDate: "2020-10-04T17:00:00.000Z",
+    title: "ImprepaRADIO",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=WE',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T17:00:00.000Z",
+    endDate: "2020-10-04T18:00:00.000Z",
+    title: "Chain",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=WE',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T19:00:00.000Z",
+    endDate: "2020-10-04T20:00:00.000Z",
+    title: "CosplayLog",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=WE',
+    room: 1
+  }
+]
+
+const THURSDAY = [
+  {
+    startDate: "2020-10-04T12:00:00.000Z",
+    endDate: "2020-10-04T13:00:00.000Z",
+    title: "Pills",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=TH',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T17:00:00.000Z",
+    endDate: "2020-10-04T18:00:00.000Z",
+    title: "WannaGuess",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=TH',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T18:00:00.000Z",
+    endDate: "2020-10-04T19:00:00.000Z",
+    title: "N.E.R.D.",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=TH',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T19:00:00.000Z",
+    endDate: "2020-10-04T21:00:00.000Z",
+    title: "HIT40UK",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=TH',
+    room: 1
+  },
+]
+
+const FRIDAY = [
+  {
+    startDate: "2020-10-04T15:00:00.000Z",
+    endDate: "2020-10-04T16:00:00.000Z",
+    title: "Polyrics",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=FR',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T16:00:00.000Z",
+    endDate: "2020-10-04T17:00:00.000Z",
+    title: "I Cinefili",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=FR',
+    room: 1
+  },
+  {
+    startDate: "2020-10-04T17:00:00.000Z",
+    endDate: "2020-10-04T18:00:00.000Z",
+    title: "Charts",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=FR',
+    room: 1
+  },
+]
+
+const SATURDAY = [
+  {
+    startDate: "2020-10-04T13:00:00.000Z",
+    endDate: "2020-10-04T14:00:00.000Z",
+    title: "Impreparadio",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=SA',
+    room: 1
+  }
+]
+
 export const SCHEDULERDATA = [
   {
-    startDate: "2020-09-24T09:45",
-    endDate: "2020-09-24T11:00",
-    title: "Meeting",
-    room: 1,
+    startDate: "2020-10-04T11:00:00.000Z",
+    endDate: "2020-10-04T12:00:00.000Z",
+    title: "Domani all'Una",
+    rRule: 'RRULE:INTERVAL=1;FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR',
+    room: 1
   },
-  {
-    startDate: "2020-09-24T12:00",
-    endDate: "2020-09-24T13:30",
-    title: "Go to a gym",
-    room: 2,
-  },
+  ...MONDAY,
+  ...TUESDAY,
+  ...WEDNESDAY,
+  ...THURSDAY,
+  ...FRIDAY,
+  ...SATURDAY
 ];
