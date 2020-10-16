ae93611947eeb23a4410ac67231434061cc58905
Giulio Mario Martena <giuliomariomartena@gmail.com>
2020-10-16 23:12:47 +0200

    Importati Redux Thunk e Logger (middleware)

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Inventary } from "./reducers/inventary";
 import { LatestRents } from "./reducers/latestRents";
 import { SchedulerData } from "./reducers/schedulerData";
 import { Styles } from "./reducers/styles";
+
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 export const ConfigureStore = () => {
   const store = createStore(
@@ -10,8 +13,9 @@ export const ConfigureStore = () => {
       schedulerData: SchedulerData,
       latestRents: LatestRents,
       inventary: Inventary,
-      styles: Styles
-    })
+      styles: Styles,
+    }),
+    applyMiddleware(thunk, logger)
   );
 
   return store;
